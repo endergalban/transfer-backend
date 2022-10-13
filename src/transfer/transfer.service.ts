312efe42c71e626408eebeb262932558ee9739a3
Ender Galban <ender.galban@b-tech.cl>
2022-10-13 09:54:50 -0300

    add receiverId validation

--- src/transfer/transfer.service.ts
+++ src/transfer/transfer.service.ts
@@ -1,16 +1,24 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTransferDto } from './dto/create-transfer.dto';
 import { Transfer } from './entities/transfer.entity';
 import { Receiver } from '../receiver/entities/receiver.entity';
+import { ReceiverService } from 'src/receiver/receiver.service';
 
 @Injectable()
 export class TransferService {
   constructor(
     @Inject('TRANSFERS_REPOSITORY')
     private transfersRepository: typeof Transfer,
+    private receiverService: ReceiverService,
   ) {}
 
   async create(createTransferDto: CreateTransferDto) {
+    const receiver = await this.receiverService.find(
+      createTransferDto.receiverId,
+    );
+    if (!receiver) {
+      throw new NotFoundException('receiverId not found');
+    }
     return this.transfersRepository.create<Transfer>({
       receiverId: createTransferDto.receiverId,
       amount: createTransferDto.amount,
